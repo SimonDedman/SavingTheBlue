ded96a4154702886e17d8d07c31246c863e0530d
Simon Dedman <simondedman@gmail.com>
2021-03-05 16:07:51 -0800

    more cleaning & mapping

--- R/01_data-import.R
+++ R/01_data-import.R
@@ -36,7 +36,7 @@ shark$Date <- as.Date(shark$Date, origin = "1899-12-30")
 ## xxx NA blank = NA
 
 # STL has ">200"
- #This was a while ago, but I’m guessing this was a shark that spit the hook and
+#This was a while ago, but I’m guessing this was a shark that spit the hook and
 # we estimated the size. For the sake of accuracy, I think it’s best to leave this
 # blank rather than guess on a specific number.
 shark$STL[which(shark$STL == ">200")] <- NA
@@ -45,9 +45,9 @@ shark$STL <- as.numeric(shark$STL)
 # Fin_genetics Fin_isotopes Muscle_isotopes: TRUE FALSE "1-day recap"
 # This indicates that samples were not collected because the shark was captured
 # the day before, so these are “False”.
-shark$Fin_genetics[which(shark$Fin_genetics == "1-day recap")] <- FALSE
-shark$Fin_isotopes[which(shark$Fin_isotopes == "1-day recap")] <- FALSE
-shark$Muscle_isotopes[which(shark$Muscle_isotopes == "1-day recap")] <- FALSE
+# shark$Fin_genetics[which(shark$Fin_genetics == "1-day recap")] <- FALSE
+# shark$Fin_isotopes[which(shark$Fin_isotopes == "1-day recap")] <- FALSE
+# shark$Muscle_isotopes[which(shark$Muscle_isotopes == "1-day recap")] <- FALSE # now done in dbase
 shark$Fin_genetics <- as.logical(shark$Fin_genetics)
 shark$Fin_isotopes <- as.logical(shark$Fin_isotopes)
 shark$Muscle_isotopes <- as.logical(shark$Muscle_isotopes)
@@ -64,28 +64,75 @@ shark$Depth_m[which(shark$Depth_m == "<1")] <- 0.5
 shark$Depth_m <- as.numeric(shark$Depth_m)
 
 # Factorial columns
-shark$Sex <- factor(shark$Sex,levels = c("F", "M"))
+shark$Sex <- factor(shark$Sex, levels = c("F", "M"))
 unique(shark$Species)
 # "G. cirratum"     "C. perezi"       "C. falciformis"  "N. brevirostris" "C. acronotus"
 # "C. limbatus"     "G. cuvier"       "C. obscurus"     "C. leucas"       "R. terraenovae"
 # "S. mokarran"
 # Will need to have something that warns us if there are new species which aren't in the existing levels list. Ditto for all of these.
 
+# PM: pelagic vs inshore then size makes the most sense:
+levels_Species <- c("C. falciformis", "C. obscurus", # silky (Carcharhinus falciformis) and dusky (Carcharhinus obscurus)
+                    "C. acronotus", "R. terraenovae", # blacknose (Carcharhinus acronotus) and sharpnose (Rhizoprionodon terraenovae)
+                    "C. limbatus", "C. perezi", "G. cirratum", # blacktip (Carcharhinus limbatus), reef (Carcharhinus perezi), and nurse (Ginglymostoma cirratum)
+                    "G. cuvier", "C. leucas", "S. mokarran") # tiger (Galeocerdo cuvier), bull (Carcharhinus leucas), hammerhead (Sphyrna mokarran)
+
+
+shark$Species <- factor(shark$Species, levels = levels_Species)
+
 unique(shark$Substrate)
-# "Sand & seagrass"   NA  "Silt & seagrass"  "Patch reef" "Sand & Rock" "Sand" "Sand & octocorals" "Silt" "Reef"
-# "Seagrass" "Hard bottom" "Sand & mud" "Coral reef" "Sand & patch reef"
+# PM: will be good to try and match with
+# sand, silt/mud, or rock
+# seagrass, algae, coral
+# coral: soft, hard
+levels_Substrate <- c("Sand & mud", "Silt", "Sand", "Sand & Rock", "Hard bottom",
+                      "Silt & seagrass", "Sand & algae", "Sand & seagrass", "Seagrass",
+                      "Sand & patch reef", "Sand & coral", "Sand & octocorals", "Patch reef", "Coral reef", "Reef")
+shark$Substrate <- factor(shark$Substrate, levels = levels_Substrate)
+
 
 unique(shark$Habitat)
-# "Channel"    "Back reef"  "Pelagic"    "Flats"      "Fore reef"  "Blue Hole "
+levels_Habitat <- c("Flats", "Channel", "Back reef", "Blue Hole", "Fore reef", "Pelagic")
+shark$Habitat <- factor(shark$Habitat, levels = levels_Habitat)
 
-unique(shark$Site)
-# "Gibson Cay" "Green Cay" "TOTO Navy Buoy" "Blackbeard's Channel" "Bigwood Channel" "Fresh Creek"
-# "Middle Bight" "AUTEC Channel" "Isla's Spot" "Bristol Galley" "AUTEC Channel Reef" "Middle Bight - upper"
-# "Middle Bight - reciever MB4" "Shark Hole" "Behring Point" "Somerset" "North Bight-upper" "High Cay"
-# "Somerset reef"
+
+shark$Site2 <- shark$Site
+unique(shark$Site2)
+shark[which(shark$Site2 == "Somerset reef"), "Site2"] <- "Somerset"
+shark[which(shark$Site2 == "AUTEC Channel Reef"), "Site2"] <- "AUTEC Channel"
+shark[which(shark$Site2 %in% c("North Bight – Upper",
+                               "Middle Bight – Upper",
+                               "Middle Bight",
+                               "Middle Bight - MB4",
+                               "Behring Point")), "Site2"] <- "North Bight"
+levels_Site2 <- c("Fresh Creek",
+                  "Somerset",
+                  "High Cay",
+                  "Green Cay",
+                  "Bristol Galley",
+                  "AUTEC Channel",
+                  "North Bight",
+                  "Shark Hole",
+                  "Isla's Spot",
+                  "Bigwood Channel",
+                  "TOTO Navy Buoy",
+                  "Blackbeard's Channel",
+                  "Gibson Cay")
+shark$Site2 <- factor(shark$Site2, levels = levels_Site2)
+
+
 
 unique(shark$Gear)
 # "Block-rig" "By-hand" "Polyball" "Handline" "Drumline-bottom" "Gillnet" "Drumline-top"
+shark$Gear2 <- shark$Gear
+shark[which(shark$Gear2 %in% c("Handline", "By-hand")), "Gear2"] <- "Hand"
+# fromhere which blockrig####
+levels_Gear2 <- c("Hand",
+                  "Polyball",
+                  "Drumline-bottom",
+                  "Drumline-top",
+                  "Gillnet")
+
 
 write.csv(x = shark,
           file = paste0("../../Data/", today(), "_shark_capture_data.csv"),
@@ -97,10 +144,10 @@ saveRDS(object = shark,
 # Drumline Data####
 
 drumline <- read.xlsx(xlsxFile = "../../Data/Shark capture data_NEW_Jan 2021_UPDATED.xlsx",
-                   sheet = 2,
-                   detectDates = TRUE, # fails
-                   check.names = TRUE,
-                   na.strings = c("NA", "xxx")) # various cols are "xxx" instead of NA or blank, why?
+                      sheet = 2,
+                      detectDates = TRUE, # fails
+                      check.names = TRUE,
+                      na.strings = c("NA", "xxx")) # various cols are "xxx" instead of NA or blank, why?
 drumline$Date <- as.Date(drumline$Date, origin = "1899-12-30")
 drumline$Depth_m <- as.numeric(drumline$Depth_m)
 
@@ -126,13 +173,38 @@ drumline$Tide <- factor(drumline$Tide, levels = c("Low", "Rising", "High", "Fall
 
 unique(drumline$Bait_type)
 # "Bonito"    "Ladyfish"  "Barracuda" "Blacknose" NA
+drumline$Bait_type <- factor(drumline$Bait_type, levels = c("Bonito", "Ladyfish", "Barracuda", "Blacknose"))
 
 unique(drumline$Substrate)
 # Same as for Shark tab, check they have the same levels e.g. hard bottom. Include all in both.
+unique(drumline$Substrate)[which(!unique(drumline$Substrate) %in% unique(shark$Substrate))]
+# "Sand & coral" "Sand & algae" # present in drumline not shark but that's fine, included in levels just not seen yet @ 2021-03-05
+drumline$Substrate <- factor(drumline$Substrate, levels = levels_Substrate)
+
+
 unique(drumline$Habitat)
 # Same as for Shark tab, check they have the same levels. Include all in both.
+# unique(drumline$Habitat)[which(!unique(drumline$Habitat) %in% unique(shark$Habitat))] # character(0)
+drumline$Habitat <- factor(drumline$Habitat, levels = levels_Habitat)
+
 unique(drumline$Site)
 # Same as for Shark tab, check they have the same levels. Include all in both.
+
+drumline$Site2 <- drumline$Site
+unique(drumline$Site2)
+drumline[which(drumline$Site2 == "Somerset reef"), "Site2"] <- "Somerset"
+drumline[which(drumline$Site2 == "AUTEC Channel Reef"), "Site2"] <- "AUTEC Channel"
+drumline[which(drumline$Site2 == "AUTEC Reef"), "Site2"] <- "AUTEC Channel"
+drumline[which(drumline$Site2 %in% c("North Bight - Upper",
+                                     "Middle Bight - Upper",
+                                     "Middle Bight",
+                                     "Middle Bight - MB4",
+                                     "Behring Point")), "Site2"] <- "North Bight"
+drumline$Site2 <- factor(drumline$Site2, levels = levels_Site2)
+
+
+
+
 unique(drumline$Bottom_top)
 drumline$Bottom_top <- factor(drumline$Bottom_top, levels = c("Bottom", "Top"))
 
--- R/02_mapping.R
+++ R/02_mapping.R
@@ -21,6 +21,13 @@ drumline <- readRDS(file = paste0("../../Data/", today(), "_drumline_data.rds"))
 
 library(ggmap)
 # Please cite ggmap if you use it! See citation("ggmap") for details.
+library(dplyr)
+library(lubridate)
+library(magrittr) # %>%
+library(sf)
+library(ggspatial)
+library(tidylog) # verbose version of tidyverse
+library(concaveman) # points to poly bounding box
 
 # bounding box lowerleftlon, lowerleftlat, upperrightlon, upperrightlat
 myLocation <- c(min(shark$Longitude), min(shark$Latitude), max(shark$Longitude), max(shark$Latitude))
@@ -29,7 +36,7 @@ myMap <- get_map(location = myLocation,
                  source = "osm",
                  maptype = "osm",
                  crop = FALSE)
-                 # zoom = 10) # default?
+# zoom = 10) # default?
 
 ggmap(myMap) +
   geom_point(aes(x = Longitude,
@@ -38,6 +45,8 @@ ggmap(myMap) +
              alpha = 0.5,
              color = "darkred",
              size = 3) +
+  annotation_spatial(sf_sites_polys, col = "black", lwd = 0.05, fill = NA) +
+  # geom_polygon(data = polys) +
   # geom_density_2d(data = shark, # doesn't work well / look good / help
   #                 aes(x = Longitude,
   #                     y = Latitude),
@@ -58,3 +67,96 @@ ggmap(myMap) +
 # Ideas / tasks
 # Heatmap for areas of high shark catch density / contours
 # Colour points by species / facet by species
+
+# Don't map silkies & duskies with the above plotting style.
+
+# Use site rather than lat/long data (see my previous email about sites Si)
+# we’ll want to choose a lat/long for each site, and from a mapping perspective probably want to
+# set the margins pretty wide so that trends are observable but specific locations are not indicated to avoid potential issues.
+#
+# could plot all the locations of individual drums on a map
+# pick a central location as the site location?
+# [make centroid from range of latlons per site]
+# [or polygon around all points]
+# focus on sampling locations with decent soak times:
+# Somerset, High Cay, Green Cay, Bristols Galley, Autec, North Bight, Shark Hole, Isla's, and Bigwood.
+site_extents <- shark %>%
+  group_by(Site2) %>%
+  summarise(npoints = n(),
+            minlat = min(Latitude, na.rm = TRUE),
+            maxlat = max(Latitude, na.rm = TRUE),
+            minlon = min(Longitude, na.rm = TRUE),
+            maxlon = max(Longitude, na.rm = TRUE),
+            meanlat = mean(Latitude, na.rm = TRUE),
+            meanlon = mean(Longitude, na.rm = TRUE))
+
+sf_shark <- sf::st_as_sf(shark, coords = c("Longitude","Latitude")) %>%
+  sf::st_set_crs(4326)
+
+polys <- st_sf(
+  aggregate( # sf_shark$geometry but filtered for sites with >3 points using site_extents lookup
+    sf_shark %>% filter(Site2 %in% (site_extents %>%
+                                      filter(npoints > 3) %>%
+                                      pull(Site2))) %>%
+      pull(geometry),
+    # same, sf_shark$Site but filtered for sites with >3 points using site_extents lookup
+    list(sf_shark %>% filter(Site2 %in% (site_extents %>%
+                                           filter(npoints > 3) %>%
+                                           pull(Site2))) %>%
+           pull(Site2)),
+    function(g){
+      st_cast(st_combine(g),"POLYGON")
+    } # from https://gis.stackexchange.com/questions/332427/converting-points-to-polygons-by-group
+  )) %>%
+  rename(Site2 = Group.1)
+
+
+
+# split sf_shark (>3) into dfs by group, into a list object
+# because site2 is a factor it breaks things
+sf_shark$Site2 <- as.character(sf_shark$Site2)
+sites_list <- split(x = sf_shark %>% filter(Site2 %in% (site_extents %>%
+                                                          filter(npoints > 3) %>%
+                                                          pull(Site2))),
+                    f = sf_shark %>% filter(Site2 %in% (site_extents %>%
+                                                          filter(npoints > 3) %>%
+                                                          pull(Site2))) %>%
+                      pull(Site2))
+
+# lapply concaveman
+sites_polys_list <- lapply(sites_list, concaveman)
+sites_polys <- data.frame(Reduce(rbind, sites_polys_list)) # loses names
+sites_polys_names <- do.call(rbind.data.frame, sites_polys_list)
+sites_polys$Site2 <- rownames(sites_polys_names)
+sf_sites_polys <- sf::st_as_sf(sites_polys, sf_column_name = "polygons") %>% # convert to sf
+  sf::st_set_crs(4326)
+
+
+
+
+# make the circles a bit bigger to reduce the resolution on the data
+# we want people to know what we're doing and some of our results, but maybe we
+# don;t want to give away too much.
+
+# For species composition, there is a cool package in R that allows you to make "scatterpies"
+# We could put a scatterpie at each site on a map and display species diversity.
+# In the attached example, you can see the size of the circle is dictated by sample size.
+# We could do the same for effort, but this would be related to overall captures of sharks per location and not CPUE of individual species.
+
+# individual heat maps for each species
+
+# CPUE using ggplot and geom_count(). This would show CPUE across sampling sites per species.
+# (individual spreadsheets with) daily CPUEs for each site.
+#  calculate the CPUE of each species per site. Then have an excel with species ID, site name, lat/lon, and CPUE
+# CPUE (sharks per hook hour) for each day of fishing at each site for each species
+
+# 0’s will need to be included for days that sharks of each species were not caught.
+# how to handle empty hooks – I don’t think it’s appropriate to consider these to be
+# fishing for the duration of the soak time since empty hooks don’t catch sharks.
+# I’ve used the precedent set by Mike that empty hooks constitute ½ of the soak time
+
+
+
+
+# Habitat: I finally got the new high res habitat data from The Nature Conservancy:
+# nearly 3gb & a weird format so will take a bit of processing but a positive step forward nonetheless
